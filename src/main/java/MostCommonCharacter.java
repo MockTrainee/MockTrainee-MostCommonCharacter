ab55c9eafa7860d949ba7d81090160595714ee40
MockTrainee <mock.trainee@aol.com>
2023-02-09 18:22:18 +0000

    Complete Monst Common Character Challenge

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +11,27 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
+        char[] characters = str.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // populate map with characters and count
+        for (int i = 0; i < characters.length; i++) {
+            if (map.containsKey(characters[i])) {
+                map.put(characters[i], map.get(characters[i]) + 1);
+            } else {
+                map.put(characters[i], 1);
+            }
+        }
+
+        // find max
+        int max =(Collections.max(map.values()));
+
+        for (Entry<Character, Integer> entry : map.entrySet()) {  
+            if (entry.getValue() == max) {
+                return entry.getKey();     
+            }
+        }
+
         return ' ';
     }
 }
